a12c47331742f7c9dc723279fe4b1508dc8faf07
Qi <1499445412@qq.com>
2021-04-29 18:04:12 +0800

    master commit

--- src/main/java/com/xxx/GitTest.java
+++ src/main/java/com/xxx/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello world");
         System.out.println("hello Git-Test");
+        System.out.println("master-Test");
     }
 }
